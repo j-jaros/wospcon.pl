d23e49ccd8ccbb2d0c481edf7df7995b280248e1
julian <jarosjulian5@gmail.com>
2023-12-10 14:16:29 +0100

    obsluga serwowania plikow, dodanie regulaminu cosplay na strone

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ app = flask.Flask(__name__, static_url_path='/static')
 def index():
     return flask.render_template("index.html")
 
-waitress.serve(app, host='127.0.0.1', port=3002)
 
+@app.route("/documents/<docname>")
+@app.route("/documents")
+@app.route("/documents/")
+def documents(docname=None):
+    if docname is None:
+        return flask.redirect("/")
+    else:
+        try:
+            return flask.send_from_directory("./documents/", f"./{docname}")
+        except Exception as ex:
+            return "Plik nie istnieje. Przepraszamy!"
+waitress.serve(app, host='127.0.0.1', port=3002)
