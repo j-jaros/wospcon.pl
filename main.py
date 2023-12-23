8b795331fe77643ec59ad82de164d54a8ecd7376
julian <jarosjulian5@gmail.com>
2023-12-24 00:27:36 +0100

    finalna aktualizacja strony.

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-import flask
-import waitress
+import threading
 
-app = flask.Flask(__name__, static_url_path='/static')
+import flask
+import pymysql
+import waitress
+import werkzeug.exceptions
+
+app = flask.Flask(__name__, static_url_path='/static', )
 
 
 @app.errorhandler(404)
-def reroute_to_index():
-    return flask.redirect("/")
+def notfound_handler(e):
+    if flask.request.method != "GET":
+        return flask.jsonify('Zasob nie zostal znaleziony')
+
+    return flask.render_template("404.html")
+
+
+@app.errorhandler(405)
+def methodunallowed_handler(e):
+    if flask.request.method != "GET":
+        return flask.jsonify(f'Metoda {flask.request.method} jest niedozwolona')
+
+    return flask.render_template("405.html")
+
+
+@app.errorhandler(500)
+def internalerror_handler(e):
+    if flask.request.method != "GET":
+        return flask.jsonify(f'Wystąpił błąd serwera.')
+
+    return flask.render_template("500.html")
 
 
 @app.route("/")
@@ -24,7 +47,27 @@ def documents(docname=None):
         try:
             return flask.send_from_directory("./documents/", f"./{docname}")
         except Exception as ex:
-            return "Plik nie istnieje. Przepraszamy!"
+            raise werkzeug.exceptions.NotFound
 
+
+connection = None
+cursor = None
+
+
+def connect_to_database():
+    global connection
+    global cursor
+
+    connection = pymysql.connect(
+        host="127.0.0.1",
+        port=3306,
+        user='root',
+        password='root',
+        database='wospcon'
+    )
+    cursor = connection.cursor()
+
+
+threading.Thread(target=connect_to_database).start()
 
 waitress.serve(app, host='0.0.0.0', port=3002)
