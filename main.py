6a7ae472ff642cb2286cd11183e429ce8405afa6
julian <jarosjulian5@gmail.com>
2023-12-24 01:23:27 +0100

    zapisywanie wejsc na strone, poprawka profilowego pana adama

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import base64
 import threading
+import time
+import traceback
 
 import flask
 import pymysql
@@ -10,6 +13,7 @@ app = flask.Flask(__name__, static_url_path='/static', )
 
 @app.errorhandler(404)
 def notfound_handler(e):
+    log(flask.request, 'error', args=str(e))
     if flask.request.method != "GET":
         return flask.jsonify('Zasob nie zostal znaleziony')
 
@@ -18,6 +22,7 @@ def notfound_handler(e):
 
 @app.errorhandler(405)
 def methodunallowed_handler(e):
+    log(flask.request, 'error', args=str(traceback.format_exc()))
     if flask.request.method != "GET":
         return flask.jsonify(f'Metoda {flask.request.method} jest niedozwolona')
 
@@ -26,6 +31,7 @@ def methodunallowed_handler(e):
 
 @app.errorhandler(500)
 def internalerror_handler(e):
+    log(flask.request, 'error', args=str(traceback.format_exc()))
     if flask.request.method != "GET":
         return flask.jsonify(f'Wystąpił błąd serwera.')
 
@@ -34,13 +40,20 @@ def internalerror_handler(e):
 
 @app.route("/")
 def index():
+    log(flask.request, 'page-access')
     return flask.render_template("index.html")
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return 'nie ma favicona :)'
 
 
 @app.route("/documents/<docname>")
 @app.route("/documents")
 @app.route("/documents/")
 def documents(docname=None):
+    log(flask.request, 'document-access', args=str(docname))
     if docname is None:
         return flask.redirect("/")
     else:
@@ -58,14 +71,34 @@ def connect_to_database():
     global connection
     global cursor
 
-    connection = pymysql.connect(
-        host="127.0.0.1",
-        port=3306,
-        user='root',
-        password='root',
-        database='wospcon'
-    )
-    cursor = connection.cursor()
+    try:
+        connection = pymysql.connect(
+            host="127.0.0.1",
+            port=3306,
+            user='root',
+            password='',
+            database='wospcon'
+        )
+        cursor = connection.cursor()
+        connection.autocommit(True)
+    except Exception as ex:
+        print(f"{time.time()} Nie mozna polaczyc do bazy danych: {ex}")
+        connection = None
+        cursor = None
+
+
+def log(req_obj, type, args=None):
+    print(f"[{time.time()}] IP: {req_obj.host} | TYPE: {type} | ARGS: {args}")
+    try:
+        if connection is None or cursor is None:
+            return
+
+        socket = req_obj.host
+        sanitized_args = base64.b64encode(bytes(str(args), 'utf-8'))
+        cursor.execute("insert into entry_log (id, socket, type, time, args) values (%s,%s,%s,%s, %s)",
+                       (0, socket, type, time.time(), sanitized_args if args is not None else None))
+    except Exception as ex:
+        print(f"{time.time()} Nie mozna zapisac loga: {ex}")
 
 
 threading.Thread(target=connect_to_database).start()
---
+++ wospcon.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 24, 2023 at 01:10 AM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wospcon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `entry_log`
+--
+
+CREATE TABLE `entry_log` (
+  `id` int(11) NOT NULL,
+  `socket` text DEFAULT NULL,
+  `type` text DEFAULT NULL,
+  `time` int(11) DEFAULT NULL,
+  `args` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `entry_log`
+--
+ALTER TABLE `entry_log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `entry_log`
+--
+ALTER TABLE `entry_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
